2a9fd59a849b0176be8d76c7ade19755531136e9
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-02 21:10:00 +0700

    day01.java

---
+++ day01.java
@@ -0,0 +1,16 @@
+public class day1 {
+    public static void main(String[] args) {
+    // ini adalah komentar single-line atau hanya satu baris
+
+    /*
+      * ini adalah komentar multi-line atau lebih dari satu baris
+    */
+
+    /**
+     * ini adalah komentar javadoc
+     */
+
+        System.out.println("day one ngoding");
+    }
+
+}
