692ec327be563d833963797530983479e527175e
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-04 10:18:38 +0700

    Update day03.java
    jenis escape sequence

--- day03.java
+++ day03.java
@@ -1,1 +1,24 @@
+public class day3 {
+    public static void main(String[] args) {
+        //Day 3
+        //Escape sequence contohnya dibawah ini 7
+
+        System.out.println("nama saya adalah all fauzhy");
+
+        System.out.println("nama saya adalah \tall fauzhy");
+
+        System.out.println("nama saya adalah \nall fauzhy");
+
+        System.out.println("nama saya adalah \"all fauzhy\"");
+
+        System.out.println("nama saya adalah \\all fauzhy");
+
+        System.out.println("nama saya adalah \fall fauzhy");
+
+        System.out.println("nama saya adalah \ball fauzhy");
+
+        System.out.println("nama saya adalah \rall fauzhy");
+
+    }
+}
 
