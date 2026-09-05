ba3949ca187113ff3200bc732dbc52fbb14e6c50
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-05 23:10:14 +0700

    Add day05.java with numeric data type examples

---
+++ day05.java
@@ -0,0 +1,20 @@
+public class day05 {
+ 
+    public static void main(String[] args) {
+    
+        double tinggi = 165.5;
+        float bb = 45;
+
+      System.out.println("=== CONTOH BILANGAN  NUMERIK===");
+      System.out.println(" tinggi badan : " + tinggi);  
+      System.out.println("berat badan : " + bb);
+
+      System.out.println("");
+
+      System.out.println("=== Nilai MIN dan MAX tipe data numerik bilangan pecahan ===");
+      System.out.println("contoh bilangan dari " + Double.MIN_VALUE+ " sampai " + Double.MAX_VALUE);
+      System.out.println("contoh bilangan dari " + Float.MIN_VALUE+ " sampai " + Float.MAX_VALUE);
+
+    }
+    
+}
