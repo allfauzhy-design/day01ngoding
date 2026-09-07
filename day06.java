ae16a33b0a189e9a3191fe9d52100070dc27cab4
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-07 13:54:32 +0700

    Create day06.java

---
+++ day06.java
@@ -0,0 +1,17 @@
+public class day06 {
+    public static void main(String[] args) {
+        char kelas = 'B';
+        boolean b = true;
+        boolean d = false;
+
+        System.out.println("char dipakai untuk menyimpan satu karakter tunggal, seperti huruf, angka, atau simbol");
+        System.out.println("saya dari kelas : " + kelas);
+
+        System.out.println("");
+
+        System.out.println("boolean dipakai untuk menyatakan nilai benar atau salah, dengan nilai true atau false");
+        System.out.println("nilai boolean : " + b);
+        System.out.println("nilai boolean : " + d);
+
+    }
+}
