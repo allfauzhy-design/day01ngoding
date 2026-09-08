93442d1ae6eec6169efb0003fb36b50c46e3343c
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-08 23:10:05 +0700

    Add day08.java with age declaration and updates

---
+++ day08.java
@@ -0,0 +1,17 @@
+public class day08 {
+    public static void main(String[] args) {
+        
+
+        //Deklarasi
+        int umur;
+
+        //Inisialisasi
+        umur = 17;
+        System.out.println("tahun 2025 umur saya : " + umur);
+
+        //Update Variable
+        umur = 18;
+        System.out.println("tahun 2026 umur saya : " + umur);
+
+    }
+}
