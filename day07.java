65bd6569dcae90c34b944c2ef08ca5d18539766d
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-08 20:57:25 +0700

    Create day07.java

---
+++ day07.java
@@ -0,0 +1,13 @@
+public class day07 {
+    public static void main(String[] args) {
+      
+      String nama = """
+            nama saya adalah all fauzhy
+            saya dari kelas B
+            nim saya D0226319
+            """;
+
+      System.out.println(nama);
+
+   }
+}
