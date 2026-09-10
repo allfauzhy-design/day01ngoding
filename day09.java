dfad2b4abef486d1b944eba8841b7cd328e876c4
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-10 21:08:39 +0700

    Add day09 class with main method and constants

---
+++ day09.java
@@ -0,0 +1,14 @@
+public class day09 {
+    public static void main(String[] args) {
+
+        // KONSTANTA (final variabel)
+
+        final String nama = "all fauzhy";
+        System.out.println("nama saya adalah : " + nama);
+
+        final double tinggiBadan = 164.5;
+        System.out.println("tinggi badan saya : " + tinggiBadan);
+        
+    }
+   
+}
