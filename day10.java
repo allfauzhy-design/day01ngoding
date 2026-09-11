28be8d50b103ef3f162b0c7963d7c9b90d311883
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-11 19:07:55 +0700

    Add Java program to display data type limits

---
+++ day10.java
@@ -0,0 +1,28 @@
+public class day10 {
+    public static void main(String[] args) {
+
+        // BATAS MINIMAL DAN MAKSIMAL DARI TIPE DATA 
+
+
+        // Tipe data byte
+        System.out.println("batas minimal dan maksimal tipe data byte adalah " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+    
+        // Tipe data int
+        System.out.println("batas minimal dan maksimal tipe data int adalah " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+    
+        // Tipe data short
+        System.out.println("batas minimal dan maksimal tipe data short adalah " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+    
+        // Tipe data long
+        System.out.println("batas minimal dan maksimal tipe data long adalah " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+    
+        // Tipe data float
+        System.out.println("batas minimal dan maksimal tipe data float adalah " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+    
+        // Tipe data double
+        System.out.println("batas minimal dan maksimal tipe data double adalah " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+
+
+
+   }
+}
