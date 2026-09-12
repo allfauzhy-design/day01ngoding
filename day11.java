c069488536a3d67e2c0ea05940a00d57166172b1
all fauzhy <allfauzhyallfauzhy@gmail.com>
2026-09-12 20:46:29 +0700

    Add basic user input functionality in day11.java

---
+++ day11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class day11 {
+    public static void main(String[] args ) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("masukkan nama anda : ");
+        String nama = input.nextLine();
+
+        System.out.print("masukkan umur anda : ");
+        int umur = input.nextInt();
+        
+        
+    }
+
+}
